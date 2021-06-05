bf4eafe1a394a345b9377722a12e92b63df3dc72
Ilham Saputrajati <ilhamsaputrajati@gmail.com>
2021-06-05 13:53:34 +0700

    auth

--- routes/web.php
+++ routes/web.php
@@ -20,17 +20,17 @@ Route::get('/', function () {
 
 Route::group(["namespace" => "Web"], function () {
 
-    Route::group(["namespace" => "User"], function () {
-        Route::group(["prefix" => "auth", "namespace" => "Auth"], function () {
-            Route::get('/register', 'RegisterController@index')->name('user.auth.register.index');
-            Route::post('/register', 'RegisterController@register')->name('user.auth.register');
-            Route::get('/login', 'LoginController@index')->name('user.auth.login.index');
-            Route::post('/login', 'LoginController@authenticate')->name('user.auth.login');
+    Route::group(["namespace" => "User", "as" => "user."], function () {
+        Route::group(["prefix" => "auth", "namespace" => "Auth", "as" => "auth."], function () {
+            Route::get('/register', 'RegisterController@index')->name('register.index');
+            Route::post('/register', 'RegisterController@register')->name('register');
+            Route::get('/login', 'LoginController@index')->name('login.index');
+            Route::post('/login', 'LoginController@authenticate')->name('login');
         });
     });
 
     Route::group(["namespace" => "Admin", "prefix" => "backyard"], function () {
-        Route::group(["prefix" => "management"], function () {
+        Route::group(["prefix" => "management", "middleware" => "auth"], function () {
             Route::get('/user', 'UserController@index')->name('admin.user.index');
         });
     });
